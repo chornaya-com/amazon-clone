31c82b24e91aedc39d02cf27c62d83e0755f2a6a
the-chornaya <me@chornaya.com>
2020-09-07 19:47:08 +0100

    small code refactoring

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
                 </Switch>
             </div>
         </Router>
-
     );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -43,7 +43,6 @@ export function Header() {
                 </div>
             </Link>
         </div>
-
     </nav>
 
 }
